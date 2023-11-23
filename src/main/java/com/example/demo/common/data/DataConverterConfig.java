9c65729df9a7a56da6c76a249beebde85e5a04bd
hisondev <64723261+hisondev@users.noreply.github.com>
2023-11-23 12:36:37 +0900

    Create DataConverterConfig.java

---
+++ src/main/java/com/example/demo/common/data/DataConverterConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.common.data;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataConverterConfig {
+    @Bean
+    @ConditionalOnMissingBean(DataConverter.class)
+    public DataConverter dataConverter() {
+        return new DataConverterDefault();
+    }
+}

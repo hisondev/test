187182b9a41f2d667d21b7333c2781e35c8bcf2d
hisondev <64723261+hisondev@users.noreply.github.com>
2023-11-23 13:22:46 +0900

    Update ApiHandler.java

--- src/main/java/com/example/demo/common/controller/ApiHandler.java
+++ src/main/java/com/example/demo/common/controller/ApiHandler.java
@@ -2,13 +2,13 @@ package com.example.demo.common.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.example.demo.common.data.DataModel;
-import com.example.demo.common.data.DataWrapper;
+import com.example.demo.common.data.model.DataModel;
+import com.example.demo.common.data.wrapper.DataWrapper;
 
 public interface ApiHandler {
     DataModel beforeHandleRequest(DataWrapper dw, HttpServletRequest req);
 
-    DataModel HandleAuthority(DataWrapper dw, HttpServletRequest req);
+    DataModel handleAuthority(DataWrapper dw, HttpServletRequest req);
 
     void handleLog(DataWrapper dw, HttpServletRequest req);
 

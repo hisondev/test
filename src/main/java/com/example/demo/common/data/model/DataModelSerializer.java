e418ab311d3a3f20807a1ce83d90569005ae5e46
hisondev <64723261+hisondev@users.noreply.github.com>
2023-11-23 13:10:43 +0900

    Create DataModelSerializer.java

---
+++ src/main/java/com/example/demo/common/data/model/DataModelSerializer.java
@@ -0,0 +1,15 @@
+package com.example.demo.common.data.model;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class DataModelSerializer extends JsonSerializer<DataModel> {
+
+    @Override
+    public void serialize(DataModel dataModelBase, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeTree(dataModelBase.getConvertJson());
+    }
+}

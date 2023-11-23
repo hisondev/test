08b367fea996f4765670da3bd996d024b364e241
hisondev <64723261+hisondev@users.noreply.github.com>
2023-11-23 13:10:31 +0900

    Create DataModelDeserializer.java

---
+++ src/main/java/com/example/demo/common/data/model/DataModelDeserializer.java
@@ -0,0 +1,20 @@
+package com.example.demo.common.data.model;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class DataModelDeserializer extends JsonDeserializer<DataModel> {
+
+    @Override
+    public DataModel deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        JsonNode node = jp.getCodec().readTree(jp);
+        DataModel dataModelBase = new DataModel(node);
+        
+        return dataModelBase;
+    }
+}

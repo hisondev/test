f7a38cc8e036432b9bec54fc1cdc22401b8282fa
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-05 16:36:37 +0900

    Update hison.js

--- src/main/resources/static/hison.js
+++ src/main/resources/static/hison.js
@@ -9,10 +9,10 @@ var Hison ={};
     Hison.custom.link = {};
     Hison.custom.link.rootUrl = 'http://localhost:8081/';
     Hison.custom.link.controllerPath = 'api';
-    Hison.custom.link.callbackError = function(error, _dw, callbackErrorFunc) {
+    Hison.custom.link.callbackError = function(error, requestDataWrapper, callbackErrorFunction) {
         console.log('error', error);
-        if(callbackErrorFunc) {
-            callbackErrorFunc(error, _dw);
+        if(callbackErrorFunction) {
+            callbackErrorFunction(error, requestDataWrapper);
         }
     };
 })();

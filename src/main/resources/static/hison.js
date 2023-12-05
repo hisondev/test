533e1b891f0de9623b444f9e1e6953710a34dd35
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-05 10:10:56 +0900

    Update hison.js

--- src/main/resources/static/hison.js
+++ src/main/resources/static/hison.js
@@ -1,7 +1,17 @@
 var Hison ={};
 (function() {
-    Hison.data = {};
-    Hison.data.convertObject = function(object) {
+    Hison.custom.data = {};
+    Hison.custom.data.convertObject = function(object) {
         return object;
     }
+
+    Hison.custom.link = {};
+    Hison.custom.link.rootUrl = 'http://localhost:8081/';
+    Hison.custom.link.controllerPath = 'api';
+    Hison.custom.link.callbackError = function(error, _dw, callbackErrorFunc) {
+        console.log('error', error);
+        if(callbackErrorFunc) {
+            callbackErrorFunc(error, _dw);
+        }
+    };
 })();

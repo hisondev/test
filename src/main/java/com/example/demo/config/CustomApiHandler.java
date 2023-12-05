25822253511c1d1325af68c0a2736c62ab203b6d
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-05 16:33:28 +0900

    Update CustomApiHandler.java

--- src/main/java/com/example/demo/config/CustomApiHandler.java
+++ src/main/java/com/example/demo/config/CustomApiHandler.java
@@ -1,7 +1,5 @@
 package com.example.demo.config;
 
-import java.util.HashMap;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,56 +14,15 @@ public class CustomApiHandler extends ApiHandlerDefault {
 
     @Override
     public DataModel beforeHandleRequest(DataWrapper dw, HttpServletRequest req) {
-        System.out.println("### This is a CustomApiControlHandler beforeHandleRequest ###");
-        HashMap<String, Object> hm = new HashMap<String, Object>();
-        hm.put("PASS", "Y");
-        
-        // String authorities = (String) session.getAttribute(sessionConfig.getAuthoritiesKey());
-        // hm.put("authorities", authorities);
-
-        DataModel dm = new DataModel(hm);
-        return dm;
-    }
-
-    @Override
-    public DataModel handleAuthority(DataWrapper dw, HttpServletRequest req) {
-        System.out.println("### This is a CustomApiControlHandler handleAuthority ###");
-        HashMap<String, Object> hm = new HashMap<String, Object>();
-        hm.put("PASS", "Y");
-        DataModel dm = new DataModel(hm);
-        return dm;
+        String httpMethod = req.getMethod();
+        if ("PUT".equals(httpMethod) || "PATCH".equals(httpMethod) || "DELETE".equals(httpMethod)) {
+            throw new SecurityException("PUT, PATCH, and DELETE requests are not allowed for security reasons.");
+        }
+        return null;
     }
 
     @Override
     public void handleLog(DataWrapper dw, HttpServletRequest req) {
-        System.out.println("### This is a CustomApiControlHandler handleLog ###");
         logHandler.handleLogging(dw, req);
     }
-
-    @Override
-    public DataWrapper handleExceptionError(Exception e, DataWrapper dw, HttpServletRequest req) {
-        System.out.println("### This is a CustomApiControlHandler handleError ###");
-        e.printStackTrace();
-
-        DataWrapper result = new DataWrapper();
-        result.put("errorMessage", e.getMessage());
-        result.put("errorCause", e.getCause() != null ? e.getCause().toString() : null);
-        result.put("errorType", e.getClass() != null ? e.getClass().getName() : null);
-        return result;
-    }
-
-    @Override
-    public DataWrapper handleThrowableError(Throwable t, DataWrapper dw, HttpServletRequest req) {
-        System.out.println("### This is a CustomApiControlHandler handleError ###");
-        t.printStackTrace();
-
-        DataWrapper result = new DataWrapper();
-        result.put("errorMessage", t.getMessage());
-        result.put("errorCause", t.getCause() != null ? t.getCause().toString() : null);
-        result.put("errorType", t.getClass() != null ? t.getClass().getName() : null);
-        return result;
-    }
-
-    @Override
-    public void afterHandleRequest(DataWrapper requestDw, DataWrapper responesDw, HttpServletRequest req) {};
 }

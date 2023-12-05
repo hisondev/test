fb8e21fea76739b8efffd277e119b43da5312a79
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-05 16:36:12 +0900

    Update dataLink.js

--- src/main/resources/static/dataLink.js
+++ src/main/resources/static/dataLink.js
@@ -1,40 +1,37 @@
 var newDataLink = (function() {
-    function DataLink(cmd, dw) {
+    function DataLink(cmd) {
         var _cmd;
-        var _dw;
         var _rootUrl = Hison.custom.link.rootUrl;
         var _controllerPath = Hison.custom.link.controllerPath;
         var _callbackErrorFunc = Hison.custom.link.callbackError;
         
-        var _vaildCheck = function(callbackWorkedFunc, callbackErrorFunc) {
+        var _vaildCheck = function(dw, callbackWorkedFunc, callbackErrorFunc) {
             if (!_cmd) {
                 throw new Error("Command not specified");
             }
-            if(_dw && (!(_dw.getType) || _dw.getType !== 'datawrapper')) {
+            if(dw && (!(dw.getType) || dw.getType() !== 'datawrapper')) {
                 throw new Error("Please insert only a valid data type.");
             }
-            if (typeof callbackWorkedFunc !== 'function') {
+            if (callbackWorkedFunc !== null && typeof callbackWorkedFunc !== 'function') {
                 throw new Error("callbackWorkedFunc must be a function");
             }
-            if (typeof callbackErrorFunc !== 'function') {
+            if (callbackErrorFunc !== null && typeof callbackErrorFunc !== 'function') {
                 throw new Error("callbackErrorFunc must be a function");
             }
-        }
+        };
 
-        var _getData = function() {
-            var dw;
-            if(_dw) {
-                dw = _dw.clone();
+        var _getDataWrapper = function(dw) {
+            if(dw) {
                 dw.putString('cmd',_cmd);
             } else {
                 dw = newDataWrapper('cmd',_cmd);
             }
             return dw;
-        }
+        };
 
-        var _request = function(methodName, callbackWorkedFunc, callbackErrorFunc) {
-            _vaildCheck(callbackWorkedFunc, callbackErrorFunc);
-            var dw = _getData();
+        var _request = function(methodName, dw, callbackWorkedFunc, callbackErrorFunc) {
+            _vaildCheck(dw, callbackWorkedFunc, callbackErrorFunc);
+            var dw = _getDataWrapper(dw);
 
             fetch(_rootUrl + _controllerPath, {
                 method: methodName,
@@ -42,61 +39,72 @@ var newDataLink = (function() {
                 body: dw.getSerialized(),
             })
             .then(response => response.json())
-            .then(result => callbackWorkedFunc(result, _dw))
-            .catch(error => _callbackErrorFunc(error, _dw, callbackErrorFunc));
+            .then(result => {
+                if(!callbackWorkedFunc) return;
+                var resultDw = null;
+                if(result) {
+                    resultDw = newDataWrapper();
+                    for(var key of Object.keys(result)) {
+                        if (result[key].constructor === Object || result[key].constructor === Array) {
+                            resultDw.putDataModel(key, newDataModel(result[key]));
+                        } else {
+                            resultDw.put(key, result[key]);
+                        }
+                    }
+                }
+                callbackWorkedFunc(resultDw, dw);
+            })
+            .catch(error => _callbackErrorFunc(error, dw, callbackErrorFunc));
         };
 
         this.get = function(resourcePath, callbackWorkedFunc, callbackErrorFunc) {
-            _vaildCheck(callbackWorkedFunc, callbackErrorFunc);
+            _vaildCheck(null, callbackWorkedFunc, callbackErrorFunc);
 
             var url = _rootUrl + resourcePath;
             fetch(url)
             .then(response => response.json())
             .then(result => callbackWorkedFunc(result))
-            .catch(error => _callbackErrorFunc(error, callbackErrorFunc));
-        }
+            .catch(error => _callbackErrorFunc(error, dataWrapper, callbackErrorFunc));
+        };
 
-        this.post = function(callbackWorkedFunc, callbackErrorFunc) {
-            _request('POST', callbackWorkedFunc, callbackErrorFunc);
-        }
+        this.post = function(requestDataWrapper, callbackWorkedFunc, callbackErrorFunc) {
+            _request('POST', requestDataWrapper, callbackWorkedFunc, callbackErrorFunc);
+        };
 
-        this.put = function(callbackWorkedFunc, callbackErrorFunc) {
-            _request('PUT', callbackWorkedFunc, callbackErrorFunc);
-        }
+        this.put = function(requestDataWrapper, callbackWorkedFunc, callbackErrorFunc) {
+            _request('PUT', requestDataWrapper, callbackWorkedFunc, callbackErrorFunc);
+        };
 
-        this.patch = function(callbackWorkedFunc, callbackErrorFunc) {
-            _request('PATCH', callbackWorkedFunc, callbackErrorFunc);
-        }
+        this.patch = function(requestDataWrapper, callbackWorkedFunc, callbackErrorFunc) {
+            _request('PATCH', requestDataWrapper, callbackWorkedFunc, callbackErrorFunc);
+        };
         
-        this.delete = function(callbackWorkedFunc, callbackErrorFunc) {
-            _request('DELETE', callbackWorkedFunc, callbackErrorFunc);
-        }
+        this.delete = function(requestDataWrapper, callbackWorkedFunc, callbackErrorFunc) {
+            _request('DELETE', requestDataWrapper, callbackWorkedFunc, callbackErrorFunc);
+        };
 
-        this.putCmd = function() {};
-
-        this.putDataWrapper = function() {};
-
-        this.set = function() {};
+        this.setCmd = function(cmd) {
+            _cmd = cmd;
+        };
 
         //create
-        if (!cmd && !dw) {
+        if (!cmd) {
             return;
         } else {
             _cmd = cmd;
-            _dw = dw;
         }
     }
     return function(keyOrObject, value) {
-        return new DataWrapper(keyOrObject, value);
+        return new DataLink(keyOrObject, value);
     };
 })();
 if(Hison) {
     Hison.custom.link.rootUrl = 'http://localhost:8081/';
     Hison.custom.link.controllerPath = 'api';
-    Hison.custom.link.callbackError = function(error, _dw, callbackErrorFunc) {
+    Hison.custom.link.callbackError = function(error, requestDataWrapper, callbackErrorFunction) {
         console.log('error', error);
-        if(callbackErrorFunc) {
-            callbackErrorFunc(error, _dw);
+        if(callbackErrorFunction) {
+            callbackErrorFunction(error, requestDataWrapper);
         }
     };
 } else {
@@ -106,10 +114,10 @@ if(Hison) {
                 rootUrl : 'http://localhost:8081/',
                 controllerPath : 'api',
             },
-            callbackError : function(error, _dw, callbackErrorFunc) {
+            callbackError : function(error, requestDataWrapper, callbackErrorFunction) {
                 console.log('error', error);
-                if(callbackErrorFunc) {
-                    callbackErrorFunc(error, _dw);
+                if(callbackErrorFunction) {
+                    callbackErrorFunction(error, requestDataWrapper);
                 }
             },
         },

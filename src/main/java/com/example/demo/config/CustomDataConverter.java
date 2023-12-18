fda8d2e252411dab15ed263c006ad2e015f82a53
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-18 16:55:09 +0900

    Update CustomDataConverter.java

--- src/main/java/com/example/demo/config/CustomDataConverter.java
+++ src/main/java/com/example/demo/config/CustomDataConverter.java
@@ -1,5 +1,10 @@
 package com.example.demo.config;
 
 import com.example.demo.common.data.converter.DataConverterDefault;
+import com.example.demo.common.data.converter.DataConverterFactory;
 
-public class CustomDataConverter extends DataConverterDefault{}
+public class CustomDataConverter extends DataConverterDefault{
+    public static void register() {
+        DataConverterFactory.setCustomConverter(new CustomDataConverter());
+    }
+}

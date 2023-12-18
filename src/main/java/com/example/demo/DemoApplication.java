b24f448f91c8daf8b3bc3d4d71fa4778efbf3987
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-18 16:55:40 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.config.CustomApiHandler;
+import com.example.demo.config.CustomDataConverter;
+
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
+		CustomApiHandler.register();
+		CustomDataConverter.register();
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }

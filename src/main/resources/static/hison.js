99aaef63c17e22a121f3baa981c19e9505448973
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-19 17:03:33 +0900

    Update hison.js

--- src/main/resources/static/hison.js
+++ src/main/resources/static/hison.js
@@ -10,10 +10,30 @@ var Hison ={};
     Hison.link.custom = {};
     Hison.link.custom.rootUrl = 'http://localhost:8081/';
     Hison.link.custom.controllerPath = 'api';
-    Hison.link.custom.callbackError = function(error, requestDataWrapper, callbackErrorFunction) {
-        console.log('error', error);
+    Hison.link.custom.timeout = 5000;
+    Hison.link.custom.BeforeGetRequst = function(methodName, resourcePath, callbackWorkedFunc, callbackErrorFunc, options, requestFunc) {
+        return requestFunc(methodName, resourcePath, callbackWorkedFunc, callbackErrorFunc, options);
+    }
+    Hison.link.custom.BeforePostRequst = function(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options, requestFunc) {
+        return requestFunc(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options);
+    };
+    Hison.link.custom.BeforePutRequst = function(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options, requestFunc) {
+        return requestFunc(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options);
+    };
+    Hison.link.custom.BeforePatchRequst = function(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options, requestFunc) {
+        return requestFunc(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options);
+    };
+    Hison.link.custom.BeforeDeleteRequst = function(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options, requestFunc) {
+        return requestFunc(methodName, requestDw, callbackWorkedFunc, callbackErrorFunc, options);
+    };
+    Hison.link.custom.callbackWorked = function(resultDw, response, callbackWorkedFunction) {
+        if(callbackWorkedFunction) {
+            callbackWorkedFunction(resultDw, response);
+        }
+    };
+    Hison.link.custom.callbackError = function(error, callbackErrorFunction) {
         if(callbackErrorFunction) {
-            callbackErrorFunction(error, requestDataWrapper);
+            callbackErrorFunction(error);
         }
     };
 })();

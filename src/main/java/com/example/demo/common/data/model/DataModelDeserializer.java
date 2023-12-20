7dc899fb7bccf395a7c5783bf0b5d8b0200dea26
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-20 13:54:25 +0900

    Update DataModelDeserializer.java

--- src/main/java/com/example/demo/common/data/model/DataModelDeserializer.java
+++ src/main/java/com/example/demo/common/data/model/DataModelDeserializer.java
@@ -1,7 +1,7 @@
 package com.example.demo.common.data.model;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -11,10 +11,19 @@ import java.io.IOException;
 public class DataModelDeserializer extends JsonDeserializer<DataModel> {
 
     @Override
-    public DataModel deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        JsonNode node = jp.getCodec().readTree(jp);
-        DataModel dataModel = new DataModel(node);
-        
+    public DataModel deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException{
+        DataModel dataModel = new DataModel();
+
+        if (jp.isExpectedStartArrayToken()) {
+            while (jp.nextToken() != JsonToken.END_ARRAY) {
+                JsonNode arrayNode = jp.readValueAsTree();
+                dataModel.addRow(arrayNode);
+            }
+        } else if (jp.getCurrentToken() == JsonToken.START_OBJECT) {
+            JsonNode objectNode = jp.readValueAsTree();
+            dataModel.addRow(objectNode);
+        }
+
         return dataModel;
     }
 }

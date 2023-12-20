ead15432562ead1ead9fae4b3629a3c3a177fdbd
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-20 13:54:50 +0900

    Update DataWrapperDeserializer.java

--- src/main/java/com/example/demo/common/data/wrapper/DataWrapperDeserializer.java
+++ src/main/java/com/example/demo/common/data/wrapper/DataWrapperDeserializer.java
@@ -1,38 +1,36 @@
 package com.example.demo.common.data.wrapper;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.NullNode;
 
 import com.example.demo.common.data.model.DataModel;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
 
 public class DataWrapperDeserializer extends JsonDeserializer<DataWrapper> {
 
     @Override
-    public DataWrapper deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        JsonNode node = jp.getCodec().readTree(jp);
+    public DataWrapper deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException{
         DataWrapper dataWrapper = new DataWrapper();
-        Iterator<Map.Entry<String, JsonNode>> fields = node.fields();
 
-        while (fields.hasNext()) {
-            Map.Entry<String, JsonNode> field = fields.next();
-            String key = field.getKey();
-            JsonNode valueNode = field.getValue();
-
-            if (valueNode.isObject() || valueNode.isArray()) {
-                DataModel dataModel = new DataModel(valueNode);
-                dataWrapper.putDataModel(key, dataModel);
-            } else if (valueNode.isNull() || valueNode instanceof NullNode) {
-                dataWrapper.put(key, null);
-            } else {
-                dataWrapper.putString(key, valueNode.asText());
+        if (jp.isExpectedStartObjectToken()) {
+            while (jp.nextToken() != JsonToken.END_OBJECT) {
+                String key = jp.getCurrentName();
+                jp.nextToken();
+                JsonToken currentToken = jp.getCurrentToken();
+                if (currentToken == JsonToken.START_OBJECT || currentToken == JsonToken.START_ARRAY) {
+                    JsonNode valueNode = jp.readValueAsTree();
+                    DataModel dataModel = new DataModel(valueNode);
+                    dataWrapper.putDataModel(key, dataModel);
+                } else if (currentToken == JsonToken.VALUE_NULL) {
+                    dataWrapper.put(key, null);
+                } else {
+                    String value = jp.getText();
+                    dataWrapper.putString(key, value);
+                }
             }
         }
 

55818ffbf776336825f8e4e2550d265b8949950c
hisondev <64723261+hisondev@users.noreply.github.com>
2023-12-21 16:55:35 +0900

    Create CachingWebSocketConfig.java

---
+++ src/main/java/com/example/demo/common/api/caching/CachingWebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.common.api.caching;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+
+@Configuration
+@EnableWebSocket
+public class CachingWebSocketConfig implements WebSocketConfigurer {
+
+    private final CachingWebSocketSessionManager sessionManager = CachingWebSocketSessionManager.getInstance();
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new CachingWebSocketHandler(sessionManager), sessionManager.getEndPoint());
+    }
+}

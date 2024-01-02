9fb9de62ff328e9ef4abe423c52b97897d8cfe41
hisondev <64723261+hisondev@users.noreply.github.com>
2024-01-02 16:55:42 +0900

    Update ServiceRuntimeException.java

--- src/main/java/com/example/demo/common/api/exception/ServiceRuntimeException.java
+++ src/main/java/com/example/demo/common/api/exception/ServiceRuntimeException.java
@@ -2,85 +2,74 @@ package com.example.demo.common.api.exception;
 
 /**
  * 
+ * Custom exception class for handling exceptions that occur within service layer operations.
+ * Extends RuntimeException and includes an additional error code for more detailed exception categorization.
+ * This class is designed to be used in conjunction with the ApiHandler's handleServiceRuntimeException method,
+ * allowing for customizable catch logic.
+ * 
+ * <p>Key Features:</p>
+ * <ul>
+ * <li>Standard runtime exception handling with the addition of an error code.</li>
+ * <li>Enables structured error handling in service layer operations.</li>
+ * <li>Integrates seamlessly with ApiController, automatically redirecting to ApiHandler's handleServiceRuntimeException for error processing.</li>
+ * </ul>
+ * <p>Usage and Integration:</p>
+ * <pre>
+ *     // Throwing a ServiceRuntimeException in service layer
+ *     &#64;Service
+ *     public class MemberService {
+ *         public DataWrapper getMember(DataWrapper dw) {
+ *             throw new ServiceRuntimeException("This is Runtime exception message test", "ERR0001");
+ *         }
+ *         ...
+ *     }
+ * 
+ *     // Handling in ApiHandler
+ *     public ResponseEntity<DataWrapper> handleServiceRuntimeException(ServiceRuntimeException e, DataWrapper dw, HttpServletRequest req) {
+ *         // Custom catch logic here
+ *     }
+ * </pre>
+ * 
+ * ServiceRuntimeException is vital for a structured approach to handling errors in the service layer,
+ * providing a clear path for managing exceptions and enhancing error response mechanisms in API operations.
+ * 
  * @author Hani son
- * @version 1.0.2
+ * @version 1.0.0
  */
 public class ServiceRuntimeException extends RuntimeException {
     private String code = "0000";
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} with the specified detail message.
-     * 
-     * @param message the detail message
-     */
     public ServiceRuntimeException(String message) {
         super(message);
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} with the specified detail message.
-     * 
-     * @param message the detail message
-     */
     public ServiceRuntimeException(String message, String code) {
         super(message);
         this.code = code;
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} with the specified detail message and cause.
-     * 
-     * @param message the detail message
-     * @param cause the cause of the exception (a {@code null} value is permitted, and indicates that the cause is nonexistent or unknown)
-     */
     public ServiceRuntimeException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} with the specified detail message and cause.
-     * 
-     * @param message the detail message
-     * @param cause the cause of the exception (a {@code null} value is permitted, and indicates that the cause is nonexistent or unknown)
-     */
     public ServiceRuntimeException(String message, Throwable cause, String code) {
         super(message, cause);
         this.code = code;
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())}.
-     * 
-     * @param cause the cause of the exception (a {@code null} value is permitted, and indicates that the cause is nonexistent or unknown)
-     */
     public ServiceRuntimeException(Throwable cause) {
         super(cause.toString(), cause);
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())}.
-     * 
-     * @param cause the cause of the exception (a {@code null} value is permitted, and indicates that the cause is nonexistent or unknown)
-     */
     public ServiceRuntimeException(Throwable cause, String code) {
         super(cause.toString(), cause);
         this.code = code;
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} using another {@code ServiceRuntimeException} as its cause.
-     * 
-     * @param cause the cause of the exception
-     */
     public ServiceRuntimeException(ServiceRuntimeException cause) {
         super(cause.toString(), cause);
     }
 
-    /**
-     * Constructs a new {@code ServiceRuntimeException} using another {@code ServiceRuntimeException} as its cause.
-     * 
-     * @param cause the cause of the exception
-     */
     public ServiceRuntimeException(ServiceRuntimeException cause, String code) {
         super(cause.toString(), cause);
         this.code = code;

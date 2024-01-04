4fd3c97b58f43fa396e9d051c2060c49ffee92d5
hisondev <64723261+hisondev@users.noreply.github.com>
2024-01-04 09:47:58 +0900

    Update LocalDateTimeDeserializer.java

--- src/main/java/com/example/demo/common/data/converter/LocalDateTimeDeserializer.java
+++ src/main/java/com/example/demo/common/data/converter/LocalDateTimeDeserializer.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * @author Hani son
- * @version 1.0.4
+ * @version 1.0.5
  */
 public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
     private DateTimeFormatter formatter;

955453b1925102e618945bbf8c51e09b542393cf
hisondev <64723261+hisondev@users.noreply.github.com>
2024-01-31 16:59:07 +0900

    Create UtilHandler.java

---
+++ src/main/java/com/example/demo/common/utils/UtilHandler.java
@@ -0,0 +1,9 @@
+package com.example.demo.common.utils;
+
+public class UtilHandler {
+    
+    public static String getPropertieFilePath() {
+        return "test-properties/";
+        // return "";
+    }    
+}

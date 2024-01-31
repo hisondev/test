0da92094fd8820deace034157d89382dbdfaaf51
hisondev <64723261+hisondev@users.noreply.github.com>
2024-01-31 16:59:27 +0900

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,29 +1,43 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
 import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+// import com.example.demo.biz.member.repository.MemberRepository;
+import com.example.demo.common.utils.Utils;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    // @Autowired
+    // private MemberRepository memberRepository;
 	@Test
 	void contextLoads() {
 		startTest();
 
-		System.out.println("test!!!");
-
+		System.out.println("addDate : " + Utils.addDate("20240131",16));
+		System.out.println("addDate : " + Utils.addDate("20240131 160024","16"));
+		System.out.println("addDate : " + Utils.addDate("2024-01-31 16:48:31",16, "h"));
+		System.out.println("addDate : " + Utils.addDate("202401",2));
+		
 		endTest();
 	}
 
-	void startTest() {
-		System.out.println("##############################");
-		System.out.println("start test!!!");
-		System.out.println("##############################");
+	public void startTest() {
+		System.out.println("#######################################################################");
+		System.out.println("Test Start!!!!!!!!!!!");
+		System.out.println("#######################################################################");
 	}
 
-	void endTest() {
-		System.out.println("##############################");
-		System.out.println("end test!!!");
-		System.out.println("##############################");
+	public void endTest() {
+		System.out.println("#######################################################################");
+		System.out.println("Test End!!!!!!!!!!!");
+		System.out.println("#######################################################################");
 	}
 }

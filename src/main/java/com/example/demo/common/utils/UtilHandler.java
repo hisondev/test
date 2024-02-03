60e12bde9ad51ed36a5d86428048d6642d8d2a32
hison0319 <hison0319@gmail.com>
2024-02-03 17:12:36 +0900

    .

--- src/main/java/com/example/demo/common/utils/UtilHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.common.utils;
-
-public class UtilHandler {
-    
-    public static String getPropertieFilePath() {
-        return "test-properties/";
-        // return "";
-    }    
-}
